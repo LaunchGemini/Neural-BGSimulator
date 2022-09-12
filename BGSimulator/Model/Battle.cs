1183aa1293b0f530e4bae89bf8db4bfce64907c9
LaunchGemini <metabaseguy@proton.me>
2022-09-12 18:52:16 +0000

    Added slider

--- BGSimulator/Model/Battle.cs
+++ BGSimulator/Model/Battle.cs
@@ -7,4 +7,25 @@ namespace BGSimulator.Model
     public class Battle
     {
         public int Round { get; set; }
-        public Player PlayerA { 
+        public Player PlayerA { get; set; }
+        public Player PlayerB { get; set; }
+        public Board PlayerABoard { get; set; }
+        public Board PlayerBBoard { get; set; }
+
+        public void Start()
+        {
+
+            var attacker = FirstAttacker();
+            var defender = attacker.CurrentMatch;
+
+            //clone again because we want to save the state at the start of the fight.
+            var attackerBoard = attacker.Board.Clone();
+            var defenderBoard = defender.Board.Clone();
+
+            attackerBoard.RivalBoard = defenderBoard;
+            defenderBoard.RivalBoard = attackerBoard;
+
+            attackerBoard.HookEvents();
+            defenderBoard.HookEvents();
+
+            attackerBoard.ApplyA
