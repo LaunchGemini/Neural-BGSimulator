38625d417e294b47e1c869b997cc98fa4d00657d
LaunchGemini <metabaseguy@proton.me>
2023-04-14 08:20:35 +0000

    Add metrics

--- BGSimulator/Model/MinionBase.cs
+++ BGSimulator/Model/MinionBase.cs
@@ -16,4 +16,29 @@ namespace BGSimulator.Model
         public int NumberOfCopies { get; set; }
         public MinionTier MinionTier { get; set; } = MinionTier.Ranks[1];
         public Attribute Attributes { get; set; } = Attribute.None;
-        public Keywords Keywords { get; set; } = Keyw
+        public Keywords Keywords { get; set; } = Keywords.None;
+        public MinionType ValidTargets { get; set; } = MinionType.All;
+        public int Level { get; set; } = 1;
+        public int Health { get; set; }
+        public int Attack { get; set; }
+
+        public int CurrentHealth
+        {
+            get
+            {
+                int currHealth = Health;
+                foreach (var buff in TempBuffs.Values)
+                {
+                    currHealth += buff.Health;
+                }
+                return currHealth - damageTaken;
+            }
+        }
+
+        public int CurrentAttack
+        {
+            get
+            {
+                int currAttack = Attack;
+                foreach (var buff in TempBuffs.Values)
+    
