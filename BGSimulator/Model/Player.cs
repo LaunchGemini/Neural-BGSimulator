df61718c94565275a277ee4000ca147f9a6f73d5
LaunchGemini <metabaseguy@proton.me>
2024-04-05 11:29:34 +0000

    sdsds

--- BGSimulator/Model/Player.cs
+++ BGSimulator/Model/Player.cs
@@ -13,4 +13,22 @@ namespace BGSimulator.Model
         public Action<Player> OnDeath = delegate { };
         const int MAX_HAND_SIZE = 10;
         const int MAX_SHOP_LEVEL = 6;
-        const int PLAYE
+        const int PLAYER_MAX_HEALTH = 40;
+
+        public Player()
+        {
+            Board = new Board() { Player = this };
+        }
+
+        public Board Board { get; set; }
+        public ITavern BobsTavern { get; set; }
+        public IBrain Brain { get; set; }
+        public Player CurrentMatch { get; set; }
+        public int Gold { get; set; } = 3;
+        public List<ICard> Hand { get; set; } = new List<ICard>();
+        public int Health { get; set; } = PLAYER_MAX_HEALTH;
+        public bool IsDead { get => Health <= 0; }
+        public Player LastMatch { get; set; }
+        public List<string> MinionsPlayedThisGame { get; private set; } = new List<string>();
+        public int MissingHealth { get { return PLAYER_MAX_HEALTH - Health; } }
+        publi
