2c2adaccf2ba45d1c2a4f9a62abb414c39097455
LaunchGemini <metabaseguy@proton.me>
2024-05-23 18:45:49 +0000

    flash

--- BGSimulator/Utils/RandomUtils.cs
+++ BGSimulator/Utils/RandomUtils.cs
@@ -1,4 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGSimulator.Utils
+{
+    public static class RandomUtils
+    {
+        public static readonly Random random = new Random();
+        private static readonly object syncLock = new object();
+
+        public static int RandomNumber(int min, int max)
+        {
+            lock (syncLock)
+            {
+                return random.Next(min, max);
+            }
+        }
+
+        public static object[] Shuffle(object[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int rnd = RandomNumber(0, array.Length);
+                var temp = array[rnd];
+                array[rnd
