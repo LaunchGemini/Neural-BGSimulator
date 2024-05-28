62d0e2f45e79064a3b0cf7acd61e6deabcd98c5a
LaunchGemini <metabaseguy@proton.me>
2024-05-28 16:17:04 +0000

    -_-

--- BGSimulator/Utils/RandomUtils.cs
+++ BGSimulator/Utils/RandomUtils.cs
@@ -26,4 +26,24 @@ namespace BGSimulator.Utils
             {
                 int rnd = RandomNumber(0, array.Length);
                 var temp = array[rnd];
-                array[rnd
+                array[rnd] = array[i];
+                array[i] = temp;
+            }
+
+            return array;
+        }
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = random.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Neural-BGSimulator
+
+Neural-BGSimulator is an open-source Hearthstone Battlegrounds simulator. It is specifically designed to work with deep learning and reinforcement learning algorithms.
+
+To learn more about Hearthstone Battlegrounds,
